2460ee7b0a70b8793452a6557aa40188010c0a45
ty <sycork@gamil.com>
2023-01-02 16:18:06 +0900

    basename

--- src/index.js
+++ src/index.js
@@ -9,12 +9,10 @@ import ScrollToTop from './components/ScrollToTop';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ScrollToTop />
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/seb41_pre_depoy">
+    <ScrollToTop />
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
